4ffb4bec872d66ebc9251d5d789b80f49cdc7990
Emmanuel Johnson <eejames@email.unc.edu>
2021-02-06 12:41:44 -0500

    Tar Heel py works now

--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -4,3 +4,16 @@ __author__ = "730273306"
 
 
 # Begin your solution here...
+b :int = print(input("Please, enter an int: "))
+c :float = b%
+if c == 2:
+    print("Tar")
+else: 
+    if c == 7:
+        print("Heels!")
+    else:
+        if c == 2 and 7:
+            print("Tar Heels!")
+        else: 
+            if c != 2 or 7:
+                print("Carolina")
